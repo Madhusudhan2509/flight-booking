5de1d96ef26b57692361d14c6158e6122b4ecbd7
Madhu <madhusudhan25092002@example.com>
2023-08-05 21:30:42 +0530

    first commit

---
+++ server/src/db/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const db = 'mongodb+srv://flight-booking:flight-booking@cluster0.hvf9qzd.mongodb.net/flight-booking?retryWrites=true&w=majority'
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log(`Connection successful`);
+}).catch((e) => {
+  console.log(`No connection: ${e}`);
+});
+
+// mongodb://localhost:27017
+
